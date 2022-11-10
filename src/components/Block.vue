dce868b4e5b3d1362da74c0fe523ffa558860cfe
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-10 14:28:16 +0800

    added style on block

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,7 @@
 <template>
-
+    <div class="block">
+        click me
+    </div>
 </template>
 
 <script>
@@ -9,5 +11,13 @@ export default {
 </script>
 
 <style>
-
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
 </style>
