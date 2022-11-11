0734549630184c878dcfe0973f7e87533b5c8f4b
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-11-11 10:28:13 +0800

    added functionality in stopTimer() function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play Now</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
 </template>
 
 <script>
